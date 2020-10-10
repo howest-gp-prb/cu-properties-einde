82f2316e405c098a7a101f296785604ed26b7664
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-10-10 12:28:38 +0200

    Add project files.

---
+++ Prb.Properties.CONS/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Prb.Properties.CORE;
+
+namespace Prb.Properties.CONS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car firstCar;
+            firstCar = new Car();
+
+            firstCar.Color = "zwart";
+            firstCar.Brand = "Hyundai";
+            firstCar.Price = 15000M;
+            // firstCar.topSpeed = -190;
+            // firstCar.SetTopSpeed(-190);
+            firstCar.TopSpeed = -190;
+
+            Car secondCar;
+            secondCar = new Car();
+
+            secondCar.Color = "wit";
+            secondCar.Brand = "Ford";
+            secondCar.Price = 9000M;
+
+            Car thirdCar = new Car("Audi", "blauw", 25000M);
+
+            decimal priceDifference = Car.PriceDifference(firstCar, thirdCar);
+
+            Console.WriteLine($"De nieuwe auto is een {firstCar.Brand}, is {firstCar.Color} van kleur en kost {firstCar.Price}");
+            Console.WriteLine("============");
+            Console.WriteLine($"Deze nieuwe auto heeft een topsnelheid van {firstCar.TopSpeed}");
+            Console.WriteLine("============");
+            Console.WriteLine($"De tweede auto is een {secondCar.Brand}, is {secondCar.Color} van kleur en kost {secondCar.Price}");
+            Console.WriteLine("============");
+            Console.WriteLine($"De derde auto is een {thirdCar.Brand}, is {thirdCar.Color} van kleur en kost {thirdCar.Price}");
+            Console.WriteLine("============");
+            Console.WriteLine($"We hebben nu {Car.CarCount} auto's");
+
+            Console.WriteLine("============");
+            Console.WriteLine($"Prijsverschil tussen {firstCar.Price} en {thirdCar.Price} is {priceDifference}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Prb.Properties.CORE/Car.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prb.Properties.CORE
+{
+    public class Car
+    {
+        //public string brand;
+        //public string color;
+        //public decimal price;
+        //public static int carCount;
+
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public static int CarCount { get; set; }
+
+        private int topSpeed;
+        public int TopSpeed
+        {
+            get { return topSpeed; }
+            set
+            {
+                if (value < 0)
+                    topSpeed = 0;
+                else
+                    topSpeed = value;
+            }
+        }
+
+        //public void SetTopSpeed(int topSpeed)
+        //{
+        //    if (topSpeed < 0)
+        //        this.topSpeed = 0;
+        //    //throw new Exception("Topsnelheid kan niet kleiner zijn dan 0");
+        //    else
+        //        this.topSpeed = topSpeed;
+        //}
+        //public int GetTopSpeed()
+        //{
+        //    return topSpeed;
+        //}
+
+        public Car() // Default constructor
+        {
+            //brand = null;
+            //color = null;
+            //price = 0;
+            //carCount++;
+            Brand = null;
+            Color = null;
+            Price = 0;
+            CarCount++;
+        }
+
+        public Car(string brand) : this() // Constructor ontvangt 1 parameter
+        {
+            //this.brand = brand;
+            Brand = brand;
+        }
+
+        public Car(string brand, string color) : this() // Constructor ontvangt 2 parameters
+        {
+            //this.brand = brand;
+            //this.color = color; 
+            Brand = brand;
+            Color = color;
+        }
+
+        public Car(string brand, string color, decimal price) : this() // Constructor ontvangt 3 parameters
+        {
+            //this.brand = brand;
+            //this.color = color;
+            //this.price = price;
+            Brand = brand;
+            Color = color;
+            Price = price;
+        }
+
+        public decimal PriceDifference(Car wagen)
+        {
+            //return Math.Abs(this.price - wagen.price);
+            return Math.Abs(this.Price - wagen.Price);
+        }
+
+        public static decimal PriceDifference(Car wagen1, Car wagen2)
+        {
+            //return Math.Abs(wagen1.price - wagen2.price);
+            return Math.Abs(wagen1.Price - wagen2.Price);
+        }
+
+        public override string ToString()
+        {
+            //return $"{brand} - {color}";
+            return $"{Brand} - {Color}";
+        }
+    }
+}
